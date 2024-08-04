e45aabb7e240bc1a1d2c570b7376ed00a6c63ea6
Diseños <150501864+l2470a@users.noreply.github.com>
2024-08-04 12:18:10 -0500

    Add files via upload

---
+++ Mensaje/script.js
@@ -0,0 +1,5 @@
+document.getElementById('boton').addEventListener('click', function() {
+    const nota = document.getElementById('nota');
+    nota.style.display = nota.style.display === 'block'? 'none' : 'block';
+  });
+  
